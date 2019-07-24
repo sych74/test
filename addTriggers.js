62eb411474cc34e79c0acc55b0eceba38cd113be
sych74 <sych74@gmail.com>
2019-07-24 22:05:41 +0300

    Update addTriggers.js

--- addTriggers.js
+++ addTriggers.js
@@ -11,8 +11,6 @@ var scaleUpLimit = resp.array[0] && resp.array[0].value ? resp.array[0].value : 
 
 if (scaleUpLimit <= scaleDownLimit) return {result:0, warning: 'autoscaling triggers have not been added due to upLimit ['+scaleUpLimit+'] <= downLimit ['+scaleDownLimit+']'}
 
-return loadGrowth.toLowerCase();
-
 if (loadGrowth.toLowerCase() == "slow") {
     var scaleUpValue = 70,
         scaleDownValue = 20,
@@ -29,6 +27,7 @@ if (loadGrowth.toLowerCase() == "fast") {
     var scaleUpValue = 30,
         scaleDownValue = 10,
         scaleNodeCount = 2;
+    return loadGrowth.toLowerCase();
 }
 
 if (cleanOldTriggers) {
