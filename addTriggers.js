3d66a4b638f746d1b8056e94aa9e614335664837
sych74 <sych74@gmail.com>
2019-07-24 17:24:56 +0300

    Create addTriggers.js

---
+++ addTriggers.js
@@ -0,0 +1,18 @@
+var APPID = getParam("TARGET_APPID"),
+    nLimitQuota;
+
+oQoutas = jelastic.billing.account.GetQuotas(APPID, session, ["environment.maxsamenodescount"]);
+
+if (oQoutas.result != 0) {
+    return oQoutas;
+}
+
+nLimitQuota = oQoutas.array[0].value;
+
+oRespTurnOn = jelastic.env.trigger.AddTrigger(APPID, session,'{"isEnabled":true,"name":"hs-add-nginx","nodeGroup":"cp","period":1,"condition":{"type":"GREATER","value":${this.greater},"resourceType":"CPU","valueType":"PERCENTAGES"},"actions":[{"type":"ADD_NODE","customData":{"limit":' + nLimitQuota + ',"count":${this.nodeCount},"notify":true}}]}');
+
+if (oRespTurnOn.result != 0) {
+    return oRespTurnOn;
+}
+
+return jelastic.env.trigger.AddTrigger(APPID, session,'{"isEnabled":true,"name":"hs-add-nginx","nodeGroup":"cp","period":5,"condition":{"type":"LESS","value":${this.less},"resourceType":"CPU","valueType":"PERCENTAGES"},"actions":[{"type":"REMOVE_NODE","customData":{"limit":2,"count":1,"notify":true}}]}');
