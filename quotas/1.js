3c5ec64415dc6e308beec4537378a98ef3b6261b
sych74 <sych74@gmail.com>
2019-11-15 11:32:42 +0200

    Update 1.js

--- quotas/1.js
+++ quotas/1.js
@@ -52,6 +52,7 @@ var f = resp.settings.fields;
 if (markup) {
          
     if (!prod && !dev){
+        f[2].values[0].disabled = true;
         f[2].values[1].disabled = true;
         f[3].hidden = false;
         f[3].markup =  "Topologies are not available. " + markup + "Please upgrade your account."
