82023b9ba52c9def343e5ad6c9fe987e191594d5
sych74 <sych74@gmail.com>
2020-05-26 01:16:05 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,6 +5,7 @@ var fields = {};
 
 for (var i = 0, field; field = jps.settings.main.fields[i]; i++)
   fields[field.name] = field;
+
 fields["le-addon"].hidden = true;
 fields["le-addon"].value = false;
 
