3a12db2e1e7b6186847da54854b8f10c973f7bbd
sych74 <sych74@gmail.com>
2022-01-21 15:49:27 +0200

    Update deployHook.js

--- deployHook.js
+++ deployHook.js
@@ -1,2 +1,10 @@
 envName = getParam('envName');
 envDomain = getParam('envDomain');
+
+var resp = jelastic.environment.control.GetEnvs(appid, session);
+if (resp.result != 0) return resp;
+
+return { 
+    result: 0,
+    resp: resp
+};
